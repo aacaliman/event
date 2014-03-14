0591098578821eb62b1aa30dd74b50d93e399c37
toanea <toanea@pitechnologies.ro>
2014-03-15 00:53:18 +0200

    Page front preview.

---
+++ sites/all/themes/events/page--front.tpl.php
@@ -0,0 +1,24 @@
+<div id="page">
+
+    <div id="header">
+        <div class="section">
+            <?php print render($page['header']); ?>
+        </div><!-- /.section-->
+    </div><!-- /#header-->
+
+    <div id="main-wrapper">
+        <div id="content">
+            <?php if ($show_messages && $messages): ?>
+                <div class="system-messages"><?php print $messages; ?></div>
+            <?php endif; ?>
+                asd
+            <?php print render($page['content']); ?>
+        </div><!-- /.#content-->
+    </div><!-- /#main-wrapper-->
+
+    <div id="footer-wrapper">
+        <?php print render($page['footer']); ?>
+    </div><!-- /#footer-wrapper-->
+
+</div><!-- /#page-->
+
